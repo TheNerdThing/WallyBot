a254269c89e51848c9de80b2d5f98aad3e986e9e
Gregory Evevsky <gregoryEvevsky@gmail.com>
2023-05-05 13:57:28 -0400

    added runSavebot to project

--- engines/wally.py
+++ engines/wally.py
@@ -4,9 +4,11 @@ sys.path.append(os.path.join(sys.path[0],'../'))
 import chess
 from chess.engine import PlayResult
 from engine_wrapper import MinimalEngine
+import runSavedBot
 
 
 class Wally(MinimalEngine):
-    
+    DEPTH = 3
     def search(self, board: chess.Board, *args: any) -> PlayResult:
-        print('hello world')
+        whiteToMove = chess.Board.turn == chess.WHITE
+        runSavedBot.find_best_move(board, DEPTH,whiteToMove,)
